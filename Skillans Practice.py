7f395932b3f0624133bf6d3b97ec48e6df465e87
RAJJOSHI <rajjoshi.pw@gmail.comI>
2025-06-29 12:28:11 +0530

    adding skillains file to github

---
+++ Skillans Practice.py
@@ -0,0 +1,164 @@
+# print ("CodeBaazigar")
+# a=10
+# print(a)
+# print(type(a))
+
+# print(11/2)
+
+# c="hello     raj how are     you"
+# print(c.strip())
+
+# d="apples"
+# e="mangoes"
+
+
+# print(d.replace(d,e))
+
+# print("my fav food is",e)
+
+# a="Raj Joshi"
+# b="Dhawal Grover"
+
+# print("My name is ",a.replace(a,b))
+
+# x=1
+# y=2
+# z=3
+
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y+z)
+# print(x%y)
+
+# print(x>0)
+# a=int(input("Enter your Age"))
+
+##################################################################################################################################
+
+####Assignment 23-06-2025
+
+##calculator
+# num1=int(input("ENTER YOUR FIRST NUMBER: "))
+
+# num2=int(input("ENTER YOUR SECOND NUMBER: "))
+# operator=(input("SELECT THE OPERATION YOU WANT TO PEFORM : For eg (+,-,*,/,%) "))
+
+
+# if(operator == "+"):
+#     print("THE OUTPUT IS : ",num1+num2)
+# elif(operator == "-"):
+#     print("THE OUTPUT IS : ",num1-num2)
+# elif(operator == "*"):
+#     print("THE OUTPUT IS : ",num1*num2)
+# elif(operator == "/"):
+#     print("THE OUTPUT IS : ",num1/num2)
+# elif(operator == "%"):
+#     print("THE OUTPUT IS : ",num1%num2)
+# else:
+#     print("YOU ENTERED A WRONG OPERATOR")
+
+
+
+##BUS TICKET FARE SYSYTEM 
+
+# name=input("ENTER YOUR NAME : ")
+# age=int(input("ENTER YOUR AGE : "))
+
+# if age <= 5:
+#     print("TICKET FOR BUS :",name,"\n"
+#           "YOUR FARE IS : FREE")
+# elif age >=5 and age <=10:
+#     print("TICKET FOR BUS :",name,"\n"
+#           "YOUR FARE IS : 20")
+# elif age >=10 and age <=20:
+#     print("TICKET FOR BUS :",name,"\n"
+#           "YOUR FARE IS : 30")
+# elif age >=20 and age <=50:
+#     print("TICKET FOR BUS :",name,"\n"
+#           "YOUR FARE IS : 100")
+# elif age >=50 and age <=70:
+#     print("TICKET FOR BUS :",name,"\n"
+#           "YOUR FARE IS : 20")
+# elif age >=70:
+#     print("TICKET FOR BUS :",name,"\n"
+#           "YOUR FARE IS : FREE")
+
+#####################################################################################################################
+
+##BUS TICKET FARE SYSYTEM ACCORDING TO DESTINATION
+
+# print("BUS WILL GO FROM HYDERDABAD TO EITHER BANGLORE, DELHI, GOA, JAIPUR OR MANGALORE. CHOOSE BETWEEN THEM")
+
+# destination=input("ENTER YOUR DESTINATION : ").lower()
+
+# if(destination == "banglore"):
+#     result="TICKET FARE IS Rs. 50"
+# elif(destination == "delhi"):
+#     result="TICKET FARE IS Rs 120"
+# elif(destination == "goa"):
+#     result="TICKET FARE IS Rs 100"
+# elif(destination == "jaipur"):
+#     result="TICKET FARE IS Rs 200"
+# elif(destination == "managalore"):
+#     result="TICKET FARE IS Rs 70"
+
+# print(result)
+
+##arrays & LOOPS
+
+# fruits= ["watermelon", "Banana" , "Apples"]
+
+# print(fruits[2])
+# fruits.reverse()
+# print(fruits[2])
+
+# fruits.insert(2,"mango")
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+# for i in range(1,101):
+#     print(i,"\n")
+
+# for i in range(0,101,2):
+#     print(i)
+
+
+##Fibonacchi Series
+# a=1
+# b=0
+# c=1
+# while(b<=100):
+#     c=a+b
+#     a=b
+#     b=c
+#     print(c)
+
+##MATRIX MAKING
+
+# for i in range(3):
+#     for j in range(4):
+#         print(1, end= " ")
+#     print()
+
+
+# for i in range(4):
+#     for j in range(4):
+#         print("*",  end = " ")
+#     print()
+
+
+''' rows = 5
+cols = 5
+for i in range(rows):
+    for j in range(cols):
+        print("*",  end = " ")
+        rows=rows-1
+    print()
+'''
+
+##################### ASSIGNMENT ################### 
+
+
